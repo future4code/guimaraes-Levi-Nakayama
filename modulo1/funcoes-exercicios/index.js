61e3c9fc5de1436975c7773eb81ce830cc183d48
levi-nakayama <levisn@gmail.com>
2021-11-18 22:42:10 -0300

    aula-funcoes

---
+++ modulo1/funcoes-exercicios/index.js
@@ -0,0 +1,84 @@
+//ex. interpretaçao de código
+//1 A - 10 e 50
+//1 B - nao vai aparecer nada, pq a variavel nao teria algum valor.
+
+//2 - A ela verifica se um elemento encontra na array, e é sensitivo.
+//2 - B i true ii - false iii - false
+
+//console.log('Eu sou o Levi, tenho 37 anos, moro em Ubatuca e nao sou estudante')
+
+//const nome = prompt('digite o seu nome')
+//const idade = Number(prompt('digite sua idade'))
+//const cidade = prompt('digite sua cidade')
+//const profissao = prompt('digite sua profissao')
+
+//console.log(`Èu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}`)
+
+//let num1 = Number(prompt('digite um valor'))
+//let num2 = Number(prompt('digite um valor'))
+//const total = num1 + num2
+
+//console.log(total)
+
+//let num3 = Number(prompt('digite um valor'))
+//let num4 = Number(prompt('digite um valor'))
+//const comparar = num3 >= num4
+//console.log(comparar)
+
+//let num5 = Number(prompt('digite um valor'))
+
+//const par = (num5%2) == 0
+
+//console.log(par)
+    
+//const mensagem = prompt('digite uma mensagem')
+
+//const mensagemNova = mensagem.toUpperCase()
+
+//console.log(mensagemNova, mensagem.length)
+
+ 
+//let num6 = Number(prompt('digite um valor'))
+//let num7 = Number(prompt('digite um valor'))
+
+//console.log(`numeros inseridos: ${num6} e ${num7}`)
+
+//const soma = num6 + num7
+//console.log(soma)
+
+//const subtracao = num6 - num7
+//console.log(subtracao)
+
+//const multi = num6 * num7
+//console.log(multi)
+
+//const divisao = num6/num7
+//console.log(divisao)
+
+//desafios
+
+//const texto = prompt('digite algo para ler')
+
+//function leitura(texto){
+
+  //  texto = texto.length; 
+  //  texto = texto.toUpperCase("")
+
+   // return texto
+
+}
+//let texto1 = leitura(texto)
+
+//console.length(texto1)
+
+//const hip = Number(prompt('digite um valor'))
+//const a = Number(prompt('digite um valor'))
+//const b = Number(prompt('digite um valor'))
+
+//const hip2 = hip*hip
+//const a2 = a*a
+//const b2 = b*b
+
+//const teorema = hip2 = a2+b2
+
+//console.log(teorema)
