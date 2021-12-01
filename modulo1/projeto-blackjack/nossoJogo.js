7d4ec94dc816c79f04bc64e03d2960641992d7ad
levi-nakayama <levisn@gmail.com>
2021-11-30 21:58:32 -0300

    exericios-lacos

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,39 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+while(confirm("quer jogar outra rodada?")) {
+
+true 
+console.log("comprando carta.")
+
+function comprarCartaUsuario(array){
+
+   for(let comprar of array) {
+
+
+   }
+
+}
+false
+console.log("game over")
+}
+
+const comprarCartaUm(carta) = { 
+nome: carta.texto
+valor = carta.valor
+}
+
+const comprarCartaDois(carta) = {
+nome: carta.texto
+valor = carta.valor
+}    
