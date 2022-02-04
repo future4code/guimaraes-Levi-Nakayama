b71660427ea744b03b525e94d8df58bfc4e3cd2e
Levi Nakayama <levisn@gmail.com>
2022-02-04 15:30:02 -0300

    atualizando branchs

---
+++ props-labedin/labedin/build/precache-manifest.c48b4e20aa73b6230334d7d3940d6786.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f6f68bbb1192fa8bde9a06857dda85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60d22b520d3804c05c1c",
+    "url": "/static/css/main.b35730db.chunk.css"
+  },
+  {
+    "revision": "c91a0bf1230c9921d3ad",
+    "url": "/static/js/2.9a4a79de.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/static/js/2.9a4a79de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d22b520d3804c05c1c",
+    "url": "/static/js/main.693b9160.chunk.js"
+  },
+  {
+    "revision": "9cec4286d4e96efb4903",
+    "url": "/static/js/runtime-main.6b07746c.js"
+  },
+  {
+    "revision": "3a91853b91995af75eb8c24b1c7da70c",
+    "url": "/static/media/levi-foto.3a91853b.jpg"
+  },
+  {
+    "revision": "bab38d6973ec801ffef91f5cdd4f9a28",
+    "url": "/static/media/marmotex.bab38d69.jpg"
+  }
+]);
