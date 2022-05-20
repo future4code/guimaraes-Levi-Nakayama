3d1159cc4685678033a82de8e2f64dc039ad5d93
Levi Nakayama <levisn@gmail.com>
2022-05-19 21:44:40 -0300

    exe-node

---
+++ node-packing-js/Node-Ex-01/index.js
@@ -0,0 +1,5 @@
+const nome = process.argv[2]
+const idade = Number(process.argv[3])
+
+console.log(`Olá, ${nome}! Você tem ${idade} anos. Em 7 anos você terá ${idade + 7}`)
+
---
+++ node-packing-js/Node-Ex-02/index.js
@@ -0,0 +1,4 @@
+const num1 = Number(process.argv[2])
+const num2 = Number(process.argv[3])
+
+console.log(`Soma é ${num1 + num2}. Subtração é ${num1 - num2}. Multiplicação é ${num1 * num2}. Divisão é ${num1 / num2}.`)
