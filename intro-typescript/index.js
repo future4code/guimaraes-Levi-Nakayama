f25eea10e6cad7f57fc320c0c5228f81d94991cf
Levi Nakayama <levisn@gmail.com>
2022-05-24 21:22:12 -0300

    intro-typescript

---
+++ intro-typescript/index.js
@@ -0,0 +1,13 @@
+function checaTriangulo(a, b, c) {
+    if (a !== b && b !== c) {
+        return "Escaleno";
+    }
+    else if (a === b && b === c) {
+        return "Equilátero";
+    }
+    else {
+        return "Isósceles";
+    }
+}
+console.log(checaTriangulo);
+checaTriangulo(5, 5, 5);
